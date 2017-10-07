96bca5ff5289ce9cc5d8d6a52a562b0e9096a7d4
Pascal Hartig <phartig@rdrei.net>
2017-10-07 20:12:59 +0100

    Implement CRC algorithm and header check

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io;
 use std::time::Duration;
 
 use serial::core::BaudRate;
+use std::iter::Iterator;
 use serial::prelude::*;
 
 fn main() {
@@ -12,10 +13,43 @@ fn main() {
     interact(&mut port).unwrap();
 }
 
-fn read_byte<T: SerialPort>(port: &mut T) -> io::Result<u8> {
-    let mut buf = vec![0_u8; 1];
+fn read_bytes<T: SerialPort>(port: &mut T) -> io::Result<[u8; 10]> {
+    let mut buf = [0_u8; 10];
     try!(port.read_exact(buf.as_mut()));
-    Ok(buf[0])
+    Ok(buf)
+}
+
+#[derive(Debug)]
+struct Message {
+    pm25: f32,
+    pm10: f32,
+}
+
+fn crc(buf: &[u8; 10]) -> u8 {
+    let s: u16 = buf[2..8].iter().map(|x| *x as u16).sum();
+    s as u8
+}
+
+fn check_crc(buf: &[u8; 10]) -> bool {
+    let cur_crc = crc(buf) as u8;
+    let nova_crc = buf[8] as u8;
+    cur_crc == nova_crc
+}
+
+fn check_header(buf: &[u8; 10]) -> bool {
+    buf[0] == 0xAA && buf[1] == 0xC0
+}
+
+fn check_message(buf: &[u8; 10]) -> bool {
+    check_header(buf) && check_crc(buf)
+}
+
+fn parse_message(buf: &[u8; 10]) -> Option<()> {
+    if check_message(buf) {
+        return None;
+    }
+
+    return Some(())
 }
 
 fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
@@ -28,9 +62,28 @@ fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
     try!(port.set_timeout(Duration::from_millis(2000)));
 
     loop {
-        let byte = read_byte(port).unwrap();
-        println!("byte: {}", byte);
+        let bytes = read_bytes(port).unwrap();
+        let msg = parse_message(&bytes);
+        println!("byte: {:?}", msg);
     }
 
     Ok(())
+}
+
+#[test]
+fn test_crc_check_passes() {
+    let buf: &[u8; 10] = &[170, 192, 13, 0, 21, 0, 64, 147, 245, 171];
+    assert_eq!(true, check_crc(buf));
+}
+
+#[test]
+fn test_crc_check_fails() {
+    let buf: &[u8; 10] = &[192, 14, 1, 21, 0, 64, 147, 246, 171, 170];
+    assert_eq!(false, check_crc(buf));
+}
+
+#[test]
+fn test_header_check_fails() {
+    let buf: &[u8; 10] = &[192, 14, 1, 21, 0, 64, 147, 246, 171, 170];
+    assert_eq!(false, check_header(buf));
 }
